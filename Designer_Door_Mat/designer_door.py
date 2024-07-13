cf4ebc46ee4e8075d51b3a488bd7cd46f7a17985
Mihretu Endeshaw <106491338+mihretue@users.noreply.github.com>
2024-07-13 12:06:59 +0300

    adding comments

--- Designer_Door_Mat/designer_door.py
+++ Designer_Door_Mat/designer_door.py
@@ -4,13 +4,13 @@ if __name__ == '__main__':
     
     M = N * 3
     width = M
-    
+    #for the first stage
     for i in range(1, N, 2):
         pattern = '.|.' * i
         print(pattern.center(width,'_'))
-        
+    #printing the text
     print('WELCOME'.center(width, '_'))
-    
+    #the last 
     for l in range(N-2, 0, -2):
         pattern = '.|.'* l
         print(pattern.center(width, '_'))
