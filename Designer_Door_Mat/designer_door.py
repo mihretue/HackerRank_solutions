68fc913c31cf6bb1ccaa5513ed1bfeac0e67cc26
Mihretu Endeshaw <106491338+mihretue@users.noreply.github.com>
2024-10-14 15:54:10 +0300

    designer for door mat

--- Designer_Door_Mat/designer_door.py
+++ Designer_Door_Mat/designer_door.py
@@ -1,17 +1,24 @@
-if __name__ == '__main__':
-    
-    N, M = map(int, input().split())
-    
-    M = N * 3
-    width = M
-    #for the first stage
-    for i in range(1, N, 2):
-        pattern = '.|.' * i
-        print(pattern.center(width,'_'))
-    #printing the text
-    print('WELCOME'.center(width, '_'))
-    #the last 
-    for l in range(N-2, 0, -2):
-        pattern = '.|.'* l
-        print(pattern.center(width, '_'))
-        
+# this is to get the number of levels that the door mat has
+
+N = int(input("please input the Number of rows"))
+M = N *3
+
+middle_pattern = '.|.'
+normal_pattern = '-'
+center_content = 'WELCOME'
+
+# for the upper part of the mat 
+for i in range(N//2):
+    pattern = middle_pattern * (2 * i + 1)
+    upper_part = pattern.center(M, normal_pattern)
+    print(upper_part)
+
+# for the middle "Welcome" text
+print(center_content.center(M,normal_pattern))
+
+# for the last part of the mat
+
+for i in range( N // 2 -1, -1, -1):
+    pattern = middle_pattern * (2 * i + 1)
+    lowwer_part = pattern.center(M, normal_pattern)
+    print(lowwer_part)
